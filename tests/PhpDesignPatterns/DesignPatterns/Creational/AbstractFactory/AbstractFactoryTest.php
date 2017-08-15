fae369274ac77ac5885d778f691675e7ee46784e
Nicklas Reincke <reincke@pier2port.de>
2017-08-15 14:28:19 +0200

    Added test for AbstractFactory

---
+++ tests/PhpDesignPatterns/DesignPatterns/Creational/AbstractFactory/AbstractFactoryTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory;
+
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\AxeInterface;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\BadAxe;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\BadSword;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\BadWeaponFactory;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\GoodAxe;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\GoodSword;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\GoodWeaponFactory;
+use PhpDesignPatterns\DesignPatterns\Creational\AbstractFactory\SwordInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TestAbstractFactory
+ *
+ * @author Nicklas Reincke <contact@reynke.com>
+ */
+class AbstractFactoryTest extends TestCase
+{
+
+    public function testAbstractFactory(): void
+    {
+        // Make good weapons! >:D
+        self::assertInstanceOf(SwordInterface::class, GoodWeaponFactory::makeSword());
+        self::assertInstanceOf(GoodSword::class, GoodWeaponFactory::makeSword());
+        self::assertInstanceOf(AxeInterface::class, GoodWeaponFactory::makeAxe());
+        self::assertInstanceOf(GoodAxe::class, GoodWeaponFactory::makeAxe());
+
+        // Make bad weapons! >:(
+        self::assertInstanceOf(SwordInterface::class, BadWeaponFactory::makeSword());
+        self::assertInstanceOf(BadSword::class, BadWeaponFactory::makeSword());
+        self::assertInstanceOf(AxeInterface::class, BadWeaponFactory::makeAxe());
+        self::assertInstanceOf(BadAxe::class, BadWeaponFactory::makeAxe());
+    }
+
+}
